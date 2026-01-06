de7673045904e1f6d1f748bb4ab14e123d99371c
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2026-01-06 10:55:30 -0300

    fix: adicionando logout

--- src/routes/MainLayout.tsx
+++ src/routes/MainLayout.tsx
@@ -2,11 +2,11 @@ import Navigation from "@/components/NavBar/Navigation";
 import { useAuth } from "@/context/AuthContext";
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
 
   return (
     <div className="min-h-screen bg-white w-screen">
-      {user && <Navigation />}
+      {user && <Navigation logout={logout} />}
       <main className="container mx-auto p-6">{children}</main>
     </div>
   );
