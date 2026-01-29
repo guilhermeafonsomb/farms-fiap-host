f9eba1f6284ed301396c88b65ce9d822f658e9f9
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2026-01-29 15:45:59 -0300

    chore: config mocker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.VITE_ENV === "dev") {
+  const { worker } = await import("./lib/mocks/browser.ts");
+  worker.start();
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
